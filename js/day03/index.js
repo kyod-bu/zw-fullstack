b076f640e829d4fe453cb069cbd33b11de74a9fc
kyod-bu <751019439@qq.com>
2021-03-26 17:44:32 +0800

    CommonJS规范学习

---
+++ js/day03/index.js
@@ -0,0 +1,4 @@
+// 为了测试模块化而准备的 index.js
+require("./moduleA");
+var m = require("./moduleB");
+console.log(m);
---
+++ js/day03/moduleB.js
@@ -0,0 +1,3 @@
+// 为了测试模块化而准备的 moduleB.js
+var m = new Date().getTime();
+module.exports = m;
