a3f65d7b69239a040ef0871d97f9a7eec9ff5fe0
kyod-bu <751019439@qq.com>
2021-09-17 06:43:13 +0800

    CommonJS 解析之导出

--- 05-node.js全栈基石/day25-node.js基础/commonjs/obj.js
+++ 05-node.js全栈基石/day25-node.js基础/commonjs/obj.js
@@ -1,22 +1,26 @@
+// 可以导出
 // exports.key = 'hello world';
-
 // exports.key2 = 'hello key2';
-
 // module.exports = 'hello world';
 
+// 不可以导出
 // exports = 'hello world';
 
 const obj = {
     key: {}
 };
 
-// obj.key = 'hello world';
+// 可以变更 obj
+// 通过 obj 的引用，去变更
+// obj.key = 'hello keykey';
 
-// var key = obj.key;
+// 可以变更 obj【原因：保持了相同的引用】
+// 将obj的引用赋值给变量`key`，去变更key的值
+const key = obj.key;
+key.key1 = 'hello key11111';
 
-// key.key1 = 'hello world';
+// 💡 不可以变更 obj
+// 原因：obj 的引用已经断掉了
+// key = 'hello world';
 
-
-key = 'hello world';
-
-console.log(obj);
+console.log('obj::', obj);
