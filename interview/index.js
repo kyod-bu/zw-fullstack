b4f660d4773e165dd757b33466846747a432ecea
kyod-bu <751019439@qq.com>
2022-01-09 03:57:48 +0800

    training

--- interview/index.js
+++ interview/index.js
@@ -2,6 +2,9 @@ const os = require('os'); // 操作系统
 const path = require('path'); // 文件路径
 const fs = require('fs'); // 文件
 
+const writeToFile = path.resolve(__dirname, './temp.csv');
+let writeToFileContent = '微信聊天对话 ID';
+
 /**
  * 递归遍历文件夹
  * @param {string} path
@@ -18,13 +21,13 @@ function getList(path) {
             const curPath22 = curPath + '/' + f;
             const curPathFiles22 = getFilesByPath(curPath22);
             if (curPathFiles22.length > 0) {
-
-
               // 聊天对象 ID：
               const chatObjId = curPath22.split('/')[14];
-              if (!arr.includes(chatObjId)) {
-                console.log('聊天对象 ID::', chatObjId);
-                console.log('聊天记录（文件）::', curPathFiles22);
+              writeToFileContent += '\n' + chatObjId + ',' + curPathFiles22.join(',');
+
+              if (!arr.includes(chatObjId)) { // 禁止读取的ID
+                // console.log('聊天对象 ID::', chatObjId);
+                // console.log('聊天记录（文件）::', curPathFiles22);
               }
             }
           }
@@ -45,11 +48,42 @@ function getFilesByPath(path) {
 }
 
 // 微信聊天记录文件夹
-// const wechat_dir = '/Users/yafbu/Library/Containers/com.tencent.xinWeChat/Data/Library/Application Support/com.tencent.xinWeChat/2.0b4.0.9/be38d38ddfe5a8c5fc1a219d60cc1491/Message/MessageTemp/131a98e70737f56c229fcbdafd0537fe/File/人选面试过的问题12.25';
-const wechat_dir = '/Users/yafbu/Library/Containers/com.tencent.xinWeChat/Data/Library/Application Support/com.tencent.xinWeChat/2.0b4.0.9/be38d38ddfe5a8c5fc1a219d60cc1491/Message/MessageTemp';
-
+const wechat_dir = path.resolve(
+  os.homedir(),
+  'Library',
+  'Containers',
+  'com.tencent.xinWeChat',
+  'Data',
+  'Library',
+  'Application Support',
+  'com.tencent.xinWeChat',
+  '2.0b4.0.9',
+  'be38d38ddfe5a8c5fc1a219d60cc1491',
+  'Message',
+  'MessageTemp',
+);
 const arr = [
   '34b96d41823b25d2a6e188266907485f', // 反盗版执行群
   '9c52501c2a777a1da6867047ebe7848b', // 视频版权保护核心
 ]
 getList(wechat_dir);
+fs.writeFileSync(writeToFile, `\ufeff${writeToFileContent}`);
+
+// ****** path 模块常用 ******
+// resolve 返回绝对路径 👍
+const resolvePath = path.resolve('..', 'a', '..', 'b', 'c/');
+// join 拼接一个路径
+const joinPath = path.join('..', 'a', '..', 'b', 'c/');
+
+console.log('resolvePath:::', resolvePath);
+console.log('joinPath:::', joinPath);
+
+// 路径处理 常用变量
+console.log('__dirname::', __dirname);
+console.log('__filename::', __filename);
+console.log('os.homedir()::', os.homedir());
+
+console.log('os.homedir()::', os.homedir());
+console.log('ext::', path.extname(__filename));
+console.log('base::', path.basename(__filename));
+console.log('dir::', path.dirname(__filename)); // 相当于 __dirname
